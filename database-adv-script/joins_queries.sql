79009634e89f2f053e283aa45d34a1aac945ec28
Mariem Tijani <mariemtijani60@gmail.com>
2025-05-17 19:42:37 +0100

    subqueries

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,17 +1,17 @@
 -- 1
-select user.first_name , user.last_name,booking.booking_id, booking.total_price
-from booking
-inner join user
-on user.user_id = booking.user_id
+SELECT user.first_name , user.last_name,booking.booking_id, booking.total_price
+FROM booking
+INNER JOIN user
+ON user.user_id = booking.user_id
 
 -- 2
-select property.name , review.rating
-from property
-left join review
-on property.property_id = review.property_id
+SELECT property.name , review.rating
+FROM property
+LEFT JOIN review
+ON property.property_id = review.property_id
 
 -- 3
-select user.first_name , booking.booking_id
-from user 
-full outer join booking
-on user.user_id = booking = user_id
+SELECT user.first_name , booking.booking_id
+FROM user 
+FULL OUTER JOIN booking
+ON user.user_id = booking = user_id
