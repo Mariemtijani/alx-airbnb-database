d3cc6de1e4be232c46347480d4f041daf5a88e7e
Mariem Tijani <mariemtijani60@gmail.com>
2025-05-23 15:29:41 +0100

    performance

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,5 @@
 -- performance.sql
+
 SELECT
     b.id AS booking_id,
     u.first_name,
@@ -10,5 +11,23 @@ SELECT
 FROM booking b
 JOIN user u ON b.user_id = u.user_id
 JOIN property p ON b.property_id = p.property_id
-JOIN payment pay ON b.booking_id = pay.booking_id;
+JOIN payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed' AND pay.amount > 100;
 
+
+
+-- Analyze performance
+EXPLAIN ANALYZE
+SELECT
+    b.id AS booking_id,
+    u.first_name,
+    u.last_name,
+    p.name AS property_name,
+    p.location,
+    pay.amount,
+    pay.payment_date
+FROM booking b
+JOIN user u ON b.user_id = u.user_id
+JOIN property p ON b.property_id = p.property_id
+JOIN payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed' AND pay.amount > 100;
